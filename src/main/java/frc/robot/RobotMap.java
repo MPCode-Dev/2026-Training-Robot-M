0e98c90703daef66e7785033f4e012dadce14e37
Michael Petrone <224322441+MPCode-Dev@users.noreply.github.com>
2025-12-18 16:09:03 -0500

    Added the shooter subsystem and shoot comand

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,20 +1,30 @@
 package frc.robot;
 
 /**
- * define Hardware Ports and CAN id's in here
+ * define Hardware Ports and CAN ids in here
  */
 public class RobotMap {
     //Drivetrain Motor Ports
 
-    //Drivetrain CAN id's
+    //Drivetrain CAN ids
 
     //Other Motor Ports
     //Elevator Motor 
     public static final int ElevatorPort1 = 9;
     public static final int ElevatorPort2 = 10;
-    //Other CAN id's
-    //Elevator CAN id's
+    //Shooter Motors
+    public static final int UpperShooterPort = 11;
+    public static final int LowerShooterPort = 12;
+    //Other CAN ids
+    //Elevator CAN ids
     public static final int ElevatorCanID1 = 9;
     public static final int ElevatorCanID2 = 10;
+    //Shooter CAN ids
+    public static final int UpperShooterCANID = 11;
+    public static final int LowerShooterCANID = 12;
+
+
+    public static final double shooterSpeed = 1*2;
+
     
 }
---
+++ src/main/java/frc/subsystems/ShooterSubststem.java
@@ -0,0 +1,25 @@
+package frc.subsystems;
+
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
+
+public class ShooterSubsystem extends SubsystemBase{
+    private TalonFX UpperMotor = new TalonFX(RobotMap.UpperShooterCanID);
+    private TalonFX LowerMotor = new TalonFX(RobotMap.LowerShooterCanID);
+     public ShooterSubsystem() {
+        UpperMotor = new TalonFX(RobotMap.UpperShooterPort);
+        LowerMotor = new TalonFX(RobotMap.LowerShooterPort);        
+    }
+
+    public void run(){
+        UpperMotor.setControl(new VoltageOut(RobotMap.shooterSpeed));
+        LowerMotor.setControl(new VoltageOut(-RobotMap.shooterSpeed));
+    }
+    public void stop(){
+        UpperMotor.setControl(new VoltageOut(0));
+        LowerMotor.setControl(new VoltageOut(0));
+    }
+}
